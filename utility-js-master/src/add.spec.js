ab00199c33da16cd5facb6f455c12daee064a110
divya kumari <divya.951127@gmail.com>
2021-08-03 14:47:26 +0530

    node

---
+++ utility-js-master/src/add.spec.js
@@ -0,0 +1,15 @@
+const add = require('./add');
+
+describe('Add', () => {
+    var expect = require('expect');
+
+
+    it('Sum of 0 and 0 is 0', () => {
+        expect(add(0, 0)).toEqual(0);
+    });
+
+    it('Sum of 1 and 0 is 1', () => {
+        expect(add(1, 0)).toEqual(0);
+    });
+
+})
